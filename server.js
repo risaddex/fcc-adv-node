0246db36c9624feaca7b2afa7a0ad47f97718b06
risaddex <risaddex@gmail.com>
2021-03-27 11:50:56 -0300

    added pug

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const app = express();
+const path = require('path')
 
 fccTesting(app); //For FCC testing purposes
+app.set('views', path.join(__dirname, 'views/pug'));
+app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
