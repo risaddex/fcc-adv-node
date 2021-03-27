a4c2de740b0fe63a6d3a6294416d4da67238c7e3
risaddex <risaddex@gmail.com>
2021-03-27 11:56:38 -0300

    passing test 1

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
-const path = require('path')
+
+app.set('view engine', 'pug')
 
 fccTesting(app); //For FCC testing purposes
-app.set('views', path.join(__dirname, 'views/pug'));
-app.set('view engine', 'pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render(process.cwd() + '/views/pug');
 });
 
 const PORT = process.env.PORT || 3000;
