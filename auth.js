c8d47735f16c550b848f9a28d3942ad6cc2556cd
risaddex <risaddex@gmail.com>
2021-03-28 09:39:26 -0300

    Implementation of Social Authentication III

--- auth.js
+++ auth.js
@@ -1,44 +1,73 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const GitHubStrategy = require('passport-github').Strategy
-const bcrypt = require('bcrypt')
-const ObjectID = require('mongodb').ObjectID
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const GitHubStrategy = require("passport-github").Strategy;
+const bcrypt = require("bcrypt");
+const ObjectID = require("mongodb").ObjectID;
 
 module.exports = function (app, myDataBase) {
-
   passport.serializeUser((user, done) => {
-    done(null, user._id)
-  })
-  
+    done(null, user._id);
+  });
+
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null,doc)
-    })
-  })
-  // Auth strategies
-  
-
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "https://tranquil-ravine-53490.herokuapp.com/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return cb(err, user);
+      done(null, doc);
     });
-  }
-));
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      myDataBase.findOne({username: username}, function (err, user) {
-        console.log(`user ${username} attempted to login`)
-        if (err) { return done(err) }
-        if (!user) { return done(null, false) }
-        if (!bcrypt.compareSync(password, user.password)) { return done(null, false) }
-        return done(null, user)
-      })
-    }
-  ))
+  });
+  // Auth strategies
 
-}
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://tranquil-ravine-53490.herokuapp.com/auth/github/callback",
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              name: profile.displayName || "John Doe",
+              photo: profile.photos[0].value || "",
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : "No public email",
+              created_on: new Date(),
+              provider: profile.provider || "",
+            },
+            $set: {
+              last_login: new Date(),
+            },
+            $inc: {
+              login_count: 1,
+            },
+          },
+          { upsert: true, new: true },
+          (err, doc) => {
+            return cb(null, doc.value);
+          }
+        );
+      }
+    )
+  );
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log(`user ${username} attempted to login`);
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+};
